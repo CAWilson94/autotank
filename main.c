3a01505170edd87717ffe6b2244f2461879f2553
charlottewilson <charlotteawilson.94@gmail.com>
2017-04-24 11:16:47 +0100

    testing shoot every five seconds

--- main.c
+++ main.c
@@ -2,10 +2,12 @@
 #include <stdio.h>
 #include <intrinsics.h>
 #include <msp430.h>
-#include <shooting.h>
+#include "shooting.h"
 int main( void )
 {
   // Stop watchdog timer to prevent time out reset
   WDTCTL = WDTPW | WDTHOLD; 
+  // Testing 
+  shoot_5_sec_interval();
   return 0;
 }
--- shooting.c
+++ shooting.c
@@ -4,13 +4,15 @@
 /* Shoot every 5 seconds repeatadly
 Can only shoot for 0.2 seconds at a time
 @C.A.Wilson
+
+Interrupt 
+Timer
 */
 void shoot_5_sec_interval()
 {
-  
+  printf("YER MAW");
 }
 
 void counter_attack()
 {
-  
 }
