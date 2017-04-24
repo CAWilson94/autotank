034f5a089ee2a0dbfa0674bf4f40f4daa3229a58
Robbie Hamill <robbierabhamill@gmail.com>
2017-04-24 09:33:13 +0100

    Adding proper project setup files

--- main.c
+++ main.c
@@ -1,10 +1,11 @@
 
-#include "io430.h"
-
+#include <stdio.h>
+#include <intrinsics.h>
+#include <msp430.h>
 int main( void )
 {
   // Stop watchdog timer to prevent time out reset
-  WDTCTL = WDTPW + WDTHOLD;
+  WDTCTL = WDTPW | WDTHOLD; // don't touch it, it's for the watchdog stuff
   // CWILSON TEST
   return 0;
 }
