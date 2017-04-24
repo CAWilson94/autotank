90e50fb3c6f7ec29231a92e6b682961190da6446
Robbie Hamill <robbierabhamill@gmail.com>
2017-04-24 11:44:34 +0100

    Added in interupt that occurs on timer, going to change to button now

--- main.c
+++ main.c
@@ -8,6 +8,16 @@ int main( void )
   // Stop watchdog timer to prevent time out reset
   WDTCTL = WDTPW | WDTHOLD; 
   // Testing 
-  shoot_5_sec_interval();
-  return 0;
+  // shoot_5_sec_interval();
+  P1DIR |= (BIT0+BIT6);   // set P1DIR with P0 and P6 to high (1)
+  P1OUT &= ~BIT6;
+  P1OUT |= BIT0; 
+  P1IES = 0; /* Set INT1 interrupt edge select reg */
+  P1IE = BIT2;   /* Set Port 1 interrupt enable reg */
+  P1IFG &= ~BIT2; //clear interrupt
+  config_timer_A();  
+  __enable_interrupt();
+  for(;;){
+    
+  }
 }
--- shooting.c
+++ shooting.c
@@ -1,6 +1,9 @@
 #include <msp430.h>
 #include <stdio.h>
 
+/////GLOBAL VARIABLES HERE////////////
+int counter = 0;
+
 /* Shoot every 5 seconds repeatadly
 Can only shoot for 0.2 seconds at a time
 @C.A.Wilson
@@ -8,11 +11,31 @@ Can only shoot for 0.2 seconds at a time
 Interrupt 
 Timer
 */
+
 void shoot_5_sec_interval()
 {
   printf("YER MAW");
 }
 
+/* Timer A0 interrupt service routine*/
+#pragma vector=TIMER0_A0_VECTOR
+__interrupt void Timer_A0 (void)
+{
+  P1OUT ^= BIT0;//&= ~BIT0;//
+    if (counter >100){
+    counter = 0;}
+    counter++;
+} 
+
+void config_timer_A(){
+      
+   
+   TA0CCTL0 = CCIE; //enable interrupts for CCRO 
+   TA0CCR0 = 16000; //6000; //set timer A0 counter target
+   TA0CTL |= TASSEL_1;	//Use ACLK as source for timer
+   TA0CTL |= MC_1;	//Use UP mode timer
+}
 void counter_attack()
 {
+  
 }
