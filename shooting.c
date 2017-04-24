1d624321b210c91be7fa7f1575498a757fd8f617
charlottewilson <charlotteawilson.94@gmail.com>
2017-04-24 10:25:02 +0100

    shooting basic function body

---
+++ shooting.c
@@ -0,0 +1,15 @@
+#include <msp430.h>
+#include <stdio.h>
+
+/* Shoot every 5 seconds repeatadly
+Can only shoot for 0.2 seconds at a time
+*/
+void shoot_5_sec_interval()
+{
+  
+}
+
+void counter_attack()
+{
+  
+}
