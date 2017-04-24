fee14d197084da8ddbedc8cbb061449f18c27b1b
Robbie Hamill <robbierabhamill@gmail.com>
2017-04-24 10:45:45 +0100

    Added functions to seperate C file, also added header file

--- main.c
+++ main.c
@@ -9,14 +9,3 @@ int main( void )
   return 0;
 }
 
-void move_body_forward(){}
-
-void move_body_backwards(){}
-
-void turn_body_right(){}
-
-void turn_body_left(){}
-
-void rotate_body_right(){}
-
-void rotate_body_left(){}
---
+++ movement.h
@@ -0,0 +1,11 @@
+void move_body_forward(){}
+
+void move_body_backwards(){}
+
+void turn_body_right(){}
+
+void turn_body_left(){}
+
+void rotate_body_right(){}
+
+void rotate_body_left(){}
