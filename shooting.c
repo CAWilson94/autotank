b63aa4259f4165501e84cbaf3bb8a2798fcea5fe
charlottewilson <charlotteawilson.94@gmail.com>
2017-04-24 12:36:39 +0100

    testing from main

--- shooting.c
+++ shooting.c
@@ -19,21 +19,23 @@ unsigned int timerCount = 0;
 // Configure timer
 void ConfigTimerA(unsigned int delayCycles)
 {
-  TA0CCTL0 |= CCIE;	/* Interrupts on Timer:"capture/compare interrupt enable*/
-  TA0CCR0 = delayCycles;/* Number of cycles in the timer counts to */
-  TA0CTL |= TASSEL_1;	/* Timer0_A3 Control: Timer A clock source select: 1 - ACLK*/
-  TA0CTL |= MC_1;	/* Timer0_A3 Control: 1 - Up to CCR0 mode */
+  TA1CCTL0 |= CCIE;	/* Interrupts on Timer:"capture/compare interrupt enable*/
+  TA1CCR0 = delayCycles;/* Number of cycles in the timer counts to */
+  TA1CTL |= TASSEL_1;	/* Timer0_A3 Control: Timer A clock source select: 1 - ACLK*/
+  TA1CTL |= MC_1;	/* Timer0_A3 Control: 1 - Up to CCR0 mode */
 }
 
 // Timer A0 interrupt service routine
 #pragma vector=TIMER0_A1_VECTOR
 __interrupt void Timer_A (void)
 {
+  
+  P1OUT ^= LED_1;
   if(timerCount >100)
   {
     timerCount = 0;
   }
-  P1OUT ^= LED_1;
+  
   timerCount++;
 }
 
@@ -41,16 +43,26 @@ __interrupt void Timer_A (void)
 void shoot_5_sec_interval()
 {
   LED_DIR |= LED_1; /*Set P1.0 to output direction*/
-  LED_OUT &= ~LED_1; /* Set LED off */
+  // LED_OUT &= ~LED_1; /* Set LED off */
   
   P1IES = 0; /* Set INT1 interrupt edge select reg */
   P1IE = INTERRUPT; /* Set Port 1 interrupt enable reg */
-  P1IFG &= ~ INTERRUPT;
+  P1IFG &= ~INTERRUPT;
   
   ConfigTimerA(3000);
   __enable_interrupt();/* Loop and wait for interrupt */
- 
+  
 }
+
+void blink_led()
+{
+  LED_DIR |= LED_1; /*Set P1.0 to output direction*/
+  LED_OUT &= ~LED_1; /* Set LED off */
+  
+  P1OUT ^= LED_1;
+  
+}
+
 
 void counter_attack()
 {
