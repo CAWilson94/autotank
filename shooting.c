a2825a8c58aaafcb1c301bd13749e0eb168d92f5
Robbie Hamill <robbierabhamill@gmail.com>
2017-04-25 14:46:34 +0100

    Had to return an int from function

--- shooting.c
+++ shooting.c
@@ -1,6 +1,10 @@
 #include <msp430.h>
 #include <stdio.h>
 
+#define LED0 BIT0
+#define LED1 BIT6
+#define BUTTON BIT3
+
 /////GLOBAL VARIABLES HERE////////////
 int counter = 0;
 
@@ -16,27 +20,26 @@ void shoot_5_sec_interval()
 {}
 
 void config_pins(){
-  P1DIR |= BIT0;   // set P1DIR with P0 to high (1)
-  P1DIR &= ~BIT3; //set P1.3 (Switch 2) as input
-  P1OUT &= ~BIT0; //turn led on
-  P1REN |= BIT3; 
-  P1OUT &= BIT3;
+  P1DIR |= LED0;   // set P1DIR with P0 to high (1)
+  P1DIR &= ~BUTTON; //set P1.3 (Switch 2) as input
+  P1OUT &= ~LED0; //turn led on
+  P1REN |= BUTTON; 
+  P1OUT &= BUTTON;
   P1IES = 0; /* Set INT1 interrupt edge select reg */
-  P1IE = BIT3;   /* Set Port 1 interrupt enable reg */
-  P1IFG &= ~BIT3; //clear interrupt 
+  P1IE = BUTTON;   /* Set Port 1 interrupt enable reg */
+  P1IFG &= ~BUTTON; //clear interrupt 
 }
-
 
 
 #pragma vector=PORT1_VECTOR
 __interrupt void sw_int(void)
 {
-  P1OUT ^= BIT0;
-  P1IFG &= ~BIT3;
+ int i = counter_attack();
+  P1IFG &= ~BUTTON;
 }
 
-
-void counter_attack()
+int counter_attack()
 {
-  
+  P1OUT ^= LED0;
+  return 1;
 }
