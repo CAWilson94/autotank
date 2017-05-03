b68e108287f8952e32dd2aefe5695eda782676cd
Robbie Hamill <robbierabhamill@gmail.com>
2017-05-03 12:36:32 +0100

    Removed unneeded comments, rdy 4 merging

--- main.c
+++ main.c
@@ -6,13 +6,4 @@ int main( void )
 {
   // Stop watchdog timer to prevent time out reset
   WDTCTL = WDTPW + WDTHOLD; 
-  /*
-  P1DIR |= BIT2;
-  P1SEL |= BIT2;
-  
-  TA0CCR0 = 26;
-  TA0CCTL1 = OUTMOD_7;
-  TA0CCR1 = 18;
-  TA0CTL = TASSEL_2 + MC_1;*/
-  shoot_5_sec_interval();
 }
