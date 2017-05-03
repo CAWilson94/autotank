679d697de1e9c0d2d97d2bc1494aec37d4c878bc
charlottewilson <charlotteawilson.94@gmail.com>
2017-05-03 12:34:44 +0100

    turret in seperate script

--- main.c
+++ main.c
@@ -11,19 +11,6 @@
 
 int main( void )
 {
-  int pirOn = 0;
-  // Stop watchdog timer to prevent time out reset
   WDTCTL = WDTPW | WDTHOLD; 
- // P1DIR |= (LED0 + LED1); // Set P1.0 to output direction 
-  P1DIR &= ~(BIT0 + BIT1 + BIT2 + BIT3);//
-
-  int i = 0;
-
-  for(;;){
-    i = (P1IN&BIT3);
-    if(P1IN&8) {
-      pirOn = 4;
-    }
-  }
 }
 
