93f82c685e6107cbbbc402486dddfa16960f9272
Robbie Hamill <robbierabhamill@gmail.com>
2017-05-03 11:38:16 +0100

    Got detection working without a port interrupt

--- main.c
+++ main.c
@@ -2,27 +2,31 @@
 #include <stdio.h>
 #include <intrinsics.h>
 #include <msp430.h>
-int main( void )
-{
-  WDTCTL = WDTPW + WDTHOLD;     // Stop WDT  
-  P1OUT &= 0x00;               // Shut down everything
-  P1DIR &= 0x00;               
-  P1DIR |= BIT0 + BIT6;            // P1.0 and P1.6 pins output the rest are input 
-  P1REN |= BIT3;                   // Enable internal pull-up/down resistors
-  P1OUT |= BIT3;                   //Select pull-up mode for P1.3
-  P1IE |= BIT3;                       // P1.3 interrupt enabled
-  P1IES |= BIT3;                     // P1.3 Hi/lo edge
-  P1IFG &= ~BIT3;                  // P1.3 IFG cleared
-  _BIS_SR(CPUOFF + GIE);          // Enter LPM0 w/ interrupt 
-  while(1)                          //Loop forever, we work with interrupts!
-  {}
-} 
-  
 
-// Port 1 interrupt service routine
-#pragma vector=PORT1_VECTOR
-__interrupt void Port_1(void)
-{    
-   P1OUT ^= BIT6;                      // Toggle P1.6
-   P1IFG &= ~BIT3;                     // P1.3 IFG cleared 
+int main(void)
+
+{
+  WDTCTL = WDTPW + WDTHOLD;            //Stop WDT
+  BCSCTL1 = CALBC1_8MHZ;                     //Set DCO to 8Mhz
+  DCOCTL = CALDCO_8MHZ;
+  P2DIR &= ~BIT0;   // IR SENSOR IS CONNECTED TO PORT2 OF BIT0
+  P1DIR |= BIT0;
+  P1OUT &= ~BIT0;
+  __delay_cycles(1000000);
+  while(1)
+  {
+  if(P2IN == 0x01) //  IF IR IS HIGH
+  {  
+    P1OUT ^= BIT0;
+  __delay_cycles(1000000);//GIVE SOME DELAY
+  }
+  else
+  {
+  __delay_cycles(1000000); // GIVE SOME DELAY
+  }
+  }
 }
+
+ 
+
+ 
