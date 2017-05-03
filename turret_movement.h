589b79b767d4f0ac5a8cb9854afe014975420183
charlottewilson <charlotteawilson.94@gmail.com>
2017-05-03 16:03:26 +0100

    turret movement methods

--- turret_movement.h
+++ turret_movement.h
@@ -8,4 +8,8 @@ void mid_point();
 void rotate_left();
 void rotate_right();
 void rotate_back();
+void rotate_front_left();
+void rotate_front_right();
+void rotate_back_left();
+void rotate_back_right();
 
